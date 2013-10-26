01029c4d3125369de1cc84ea07a90877ad1c803f
Nadav Spiegelman <me@nadav.name>
2013-10-25 22:35:44 -0400

    Add function to change body border with scroll

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -34,3 +34,27 @@ $(document).ready(function(){
 $(document).ready(function(){
   $("#home-quote").fadeTo(0,0).delay(800).fadeTo(800,1);
 });
+
+$(document).ready(function(){
+  var $window, bodyBorder, sections;
+  $window = $(window);
+  bodyBorder = $('.body-border');
+  sections = $('section').get().reverse();
+  return $window.scroll(function() {
+    var section, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = sections.length; _i < _len; _i++) {
+      section = sections[_i];
+      if ($window.scrollTop() - $(section).offset().top > -100) {
+        // colorChange.css({
+        //   'background-color': '#' + $(section).attr('id')
+        // });
+          bodyBorder.css({"background-color": $(section).data("bgcolor")});
+        break;
+      } else {
+        _results.push(void 0);
+      }
+    }
+    return _results;
+  });
+});
